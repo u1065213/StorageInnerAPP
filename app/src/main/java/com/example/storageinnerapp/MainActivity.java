9cf657bb05386b3fb55988bc92b255fb440c6c7b
u1065213 <u1065213>
2020-06-22 11:57:19 +0800

    123123

--- app/src/main/java/com/example/storageinnerapp/MainActivity.java
+++ app/src/main/java/com/example/storageinnerapp/MainActivity.java
@@ -4,16 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
     private  String filename = "myNotc.txt";
     private  static  final int READ_BLOCK_SINE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,4 +43,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-}
+    public void btnREAD(View view) {
+        try{
+            FileInputStream ips = openFileInput(filename);
+            InputStreamReader ipr = new InputStreamReader(ips);
+            char[]buffer = new char[READ_BLOCK_SINE];
+            String str = "";
+            int count;
+            while ((count=ipr.read(buffer))>0){
+                String s = String.copyValueOf(buffer,0,count);
+                str += s;
+                buffer=new char[READ_BLOCK_SINE];
+            }
+            ips.close();
+            Toast.makeText(this, "Save 成功", Toast.LENGTH_LONG).show();
+            TextView txvShow = (TextView)findViewById(R.id.textView2);
+            txvShow.setText("Read 內容：\n" + str);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    }
+
